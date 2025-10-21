5dff3a18c00c0ef5ff3098a07dae0931fc05509f
samihakallel0206 <samkallel@hotmail.fr>
2025-10-21 18:01:19 +0100

    ws-js-21-10

--- index.js
+++ index.js
@@ -1,413 +1,48 @@
-// alert("Bonjour JS!");
-// ***************!Logging output & Console
-//  alert("Hello World"); //  Do not use for debugging. Stops the script!
-// console.log("bonjour je suis la ");
-// console.error("This is an error");
-// console.warn("This is a warning");
-// console.log(prompt("age"));
-
-//***********************!-Variable
-
-// var name = "student";
-// console.log(name);
-// var name = "instructor";
-// console.log(name);
-
-// const name = "someone";
-// const age = 99;
-// console.log(age);
-// var can be re-assigned, const can not
-// name = "someone2";
-// age = 100;
-// console.log(age);
-
-//*****************************************!Data types
-
-// var name = "a";
-// console.log(typeof name);
-// var age = 260;
-// console.log(typeof age);
-// var tallness = 1.75;
-// console.log(typeof tallness);
-
-// var isPerson = false;
-// console.log(typeof isPerson);
-// var x = null;
-// console.log(typeof x);
-// var z;
-// console.log(typeof z);
-
-//**************************!Arthmetic Operators
-// + , - , * , / , % , ++ , -- , **
-
-// console.log(13 - 2);
-// console.log("3" * "2");
-// console.log(typeof "3");
-// console.log(3 * "TEXT");
-// console.log("10" / "2");
-// console.log(10 % 2);
-// console.log(10 % 3);
-// console.log("3" ** "2"); //puissance
-
-// console.log(5 + 3);
-// console.log(5 + "a"); //concatenation
-
-// var y = 5;
-// console.log(++y);
-// console.log(y)
-// console.log(y++);
-// console.log(y);
-// var z;
-// z = ++y;
-// console.log(z);
-// console.log(y);
-
-//******************!Comparaison operators
-// = affectation d'une vvaleur à une variable
-
-// == comparer les valeur des variables
-
-// === comparer valeurs et types variables
-
-// / ! le contraire (opposite)
-
-// /!= comparer l'inégalité des valeurs
-
-// /!== compares the inequality based on value and type
-
-// var a = 25;
-// var b = "25";
-// console.log(typeof a);
-// console.log(typeof b);
-// if (a !== b) {
-//   console.log(a + " equal: value and type " + b);
-// } else {
-//   console.log("not equal in type or value");
-// }
-// if (a != b) {
-//   console.log("equal ");
-// } else {
-//   console.log("not equal");
-// }
-
-//****************************!Truthy & Falsy
-// false, undefined, null, 0, "", NaN
-
-// var test = null;
-// var title = "";
-// var y;
-// console.log(typeof y);
-// var a = 0;
-// console.log(test);
-// console.log(Boolean(test));
-// console.log(Boolean(title));
-// console.log(Boolean(y));
-// console.log(Boolean(a));
-// if (test) {
-//   console.log('okay')
-// } else {
-//   console.log(' not okay')
-// }
-
-// ***********************************!logical Operators
-// ********** X && Y :and
-// Returns first value if falsy
-// console.log(null && 5);
-// console.log(5 && 1);
-
-// ********** X || Y :OR
-// Returns first value if truthy
-// console.log(1 || 0);
-
-//***************!String concatenation
-// var name = "hamza";
-// var age = 20;
-
-// Archaic method
-// console.log("My name is " + name + "\n" + "and I am " + age);
-// Template literal (use this one!)
-// console.log(`My name is ${name}
-//     and I am
-//      ${2 + 3}`);
-
-//Special Characters (\, \', \n, \\)
-
-// console.log("je m'appelle sam");
-
-//******!String methods & properties **** */
-var s = "Hello World";
-// var val;
-// console.log(val);
-// console.log(typeof val);
-
-// Get length
-// val = s.length;
-// console.log(s.length);
-// console.log(val);
-
-// String Index
-// console.log(s[0])
-
-// Change case
-// val = s.toUpperCase()
-// console.log(val)
-// val = s.toLowerCase()
-// console.log(val)
-
-// // Get sub string
-// val = s.substring(0, 5)
-// console.log(val)
-
-// Split into array
-val = s.split(' ') //result array
-console.log(val)
-// val=[1,4,5]
-// val = val.join('')// array ----> string
-// console.log(val)
-
-// console.log(s)
-// console.log(val)
-
-//*****************!Switch (/!\break)    */
-// var color = "red";
-// switch (color) {
-//   case "red":
-//     console.log("color is red");
-//   break;
-//   case "blue":
-//     console.log("color is blue");
-//   break;
-//   case "yellow":
-//     console.log("color is yellow");
-//     break;
-//   default:
-//     console.log("color is neither red nor blue");
-// }
-// reproducing the conditional with if else
-// if (color === "red") {
-//   console.log("color is red");
-// } else if (color === "blue") {
-//   console.log("color is blue");
-// } else {
-//   console.log("color is neither red nor blue");
-// }
-
-// ************!FUNCTIONS (as a type)
-// function dummyFunc() {}
-// // Check type
-// console.log(typeof dummyFunc)
-
-//************      Functions       */
-// function greeting(n) {
-//   return 'Hello, my name is ' + n
-// }
-// console.log(greeting('Nissrine'))
-// console.log(greeting('Wissem'))
-// console.log(greeting(5))
-
-// function sub(g, h) {
-//   return g - h;
-// }
-// console.log(sub(4, 3));
-// console.log(sub(600, 326587));
-// console.log(sub(y, x));
-
-// function add(g, h) {
-//   return g + h;
-// }
-// console.log(add("4", "3"));
-
-// declaration of array
-// var tab = [];
-// console.log(tab);
-// console.log(typeof tab);
-
-//************* */ Conditionals
-// **** Simple If/Else Statement
-// const x = "10";
-
-// if (x === 10) {
-//   console.log("x is 10");
-// } else if (x > 10) {
-//   console.log("x is greater than 10");
-// } else {
-//   console.log("x is less than 10");
-// }
-
-//*********************!!loops
-// ********************** For
-// for (var i = 1; i <= 10; i++) {
-//   console.log("For Loop Number: " + i);
-// }
-
-// var tab = [1, "p", "hello"];
-// // for (var i = 0; i <= tab.length - 1; i++) {
-// for (var i = tab.length - 1; i >= 0; i--) {
-//   console.log(tab[i]);
-// }
-/*    for ... of   */
-// for (var el of tab) {
-//   ///value ou elment
-//   console.log(el);
-// }
-// /*       for ... in       */
-// for (var i in tab) {
-//   // index
-//   console.log(i);
-//   console.log(tab[i]);
-// }
-//*********************!While
-// var l = 3
-// while (l < 5) {
-//   console.log('hola')
-//   l++
-// }
-// var i = 1
-// do {
-//   console.log(`Do While Loop Number: ${i}`)
-//   i++
-// } while (i <= 10)
-
-// ************!ARRAYS - Store multiple values in a variable
-// var numbers = [1, 2, 3, 4, 5];
-// var fruits = ['apples', 'oranges', 'pears', 'grapes'];
-// var hobbies = ["sleep", 33, true];
-
-// console.log(numbers);
-// console.log(fruits);
-// console.log(hobbies);
-// console.log(typeof numbers);
-// *** Tables
-// const numbers = [1,2,3]
-// console.log(numbers);
-// Still an assignment ! (with a new table)
-// numbers = [2,3]
-
-// numbers[1] = 'hello'
-
-// console.log(numbers)
-
-//********************! Array Methods & Properties
-
-// const colors = ['black', 'white', 'red', 'blue']
-// console.log(colors)
-
-// Get length
-// console.log(colors.length)
-
-// Get one value (!: Arrays start at 0)
-// console.log(colors[3])
-// Add value using push()
-// colors.push('yellow')
-// console.log(colors);
-// // Add to beginning
-// colors.unshift('purple')
-// console.log(colors);
-
-// // Remove last value
-// colors.pop()
-// console.log(colors);
-
-// // Remove first value
-// colors.shift()
-// console.log(colors);
-
-// // Check if array
-// console.log(Array.isArray(colors))
-
-// // Get index
-// console.log(colors.indexOf(''))
-
-// console.log(colors)
-
-// **********************!Loop Through Arrays
-// Array of objects
-// const numbers = [22, 55, 66];
-// // // ********************** For Loop
-// for (var i = 0; i < numbers.length; i++) {
-// console.log(numbers[i]);
-// }
-
-// // // ********************** For...of Loop
-// for (var x of numbers) {
-// console.log(x);
-// }
-
-// // // ********************** For...in Loop
-
-// for (var i in numbers) {
-// console.log(numbers[i]);
-
-// }
-
-// ************! OBJECTS LITERALS
-// const person = {
-// name: "Eve",
-// age: 5000,
-// isPerson: true,
-// hobbies: ["eating", "sleeping"]
-// };
-
-// console.log(person);
-// console.log(person.name);
-
-//****************************!Object Literals
-// const person = { // an object is a collection of key-value pairS
-// firstName: 'Adam',
-// age: 30,
-// hobbies: ['music', 'movies', 'sports'],
-// address: {
-// street: '66 Main st',
-// city: 'wtvr',
-// state: 'over there'
-// }
-// };
-
-// Get single value (obj.key)
-// console.log(person.firstName)
-// console.log(person.address.street)
-// Add property
-// person.email = 'adam@gmail.com';
-// person.hobbies.push('singing');
-// console.log(person);
-
-// const Mutability
-// *** Objects
-// const person = {
-// name: 'foulan',
-// age:27,
-// adresse:'tunis'}
-// console.log(person);
-// // Change a property
-// person.name = 'mohamed'
-// console.log(person);
-
-// // // delete a property
-// delete(person.age)
-// console.log(person);
-
-// // Add a property
-// person.email = 'm@gmail.com';
-// console.log(person)
-
-// ************ /!\ Array of objects
-// const users = [
-// {
-// name: 'person',
-// age: 23,
-// email: "p1@gmail.com",
-// },
-// {
-// name: 'person 2',
-// age: 31,
-// email: "p2@gmail.com",
-// },
-// {
-// name: 'person 3',
-// age: 155,
-// email: "p3@gmail.com",
-// }
-// ];
-
-// console.table(users)
-// console.log(users);
+// Mettre les mots en majuscule : implémentez une fonction qui met en majuscule la première lettre de chaque mot d'une phrase.
+
+// console.log(5);
+"bonjour chers collègues";
+"Bonjour Chers Collègues";
+
+function majMot(chaine) {
+  var str = chaine[0].toUpperCase();
+
+  for (var i = 1; i < chaine.length; i++) {
+    str = str + chaine[i];
+  }
+  return str;
+}
+// console.log(majMot("hello"));
+function MajPh(phrase) {
+  var resF = "";
+  var tab = phrase.split(" ");
+  console.log(tab.join(" "));
+  console.log(tab);
+  for (var mot of tab) {
+    console.log(mot);
+    resF += majMot(mot) + " ";
+  }
+  return resF;
+}
+
+// console.log(MajPh("bonjour chers collègues"));
+
+tab = [1, 5, 0, 4, -8, -2];
+
+function MinMax(tab) {
+  var min = tab[0];
+  var max = tab[0];
+  for (var el of tab) {
+    if (min > el) min = el;
+    if (max < el) max = el;
+  }
+
+  return `le minimum du tableau est ${min} et le max est ${max}`;
+}
+
+console.log(MinMax(tab));
+
+sum = 0;
+for (var elt of tab) {
+  sum += elt;
+}
--- main.js
+++ main.js
@@ -1,197 +1,159 @@
-// alert("Bonjour les amis!");
-// ***************!Journalisation de sortie & Console
-// alert("Hello World"); // Ne pas utiliser pour le débogage!!!
-// // console.log("bonjour je suis là");
-// console.error("Ceci est une erreur");
+// !JOURNALISATION ET CONSOLE
+
+// alert("Bonjour tous le monde!"); // Ne pas utiliser pour le débogage
+// console.log("bonjour je suis là");
 // console.warn("Ceci est un avertissement");
+// console.error("Ceci est une erreur");
 // console.log(prompt("âge"));
 
-//***********************!-Variable
+//!VARIABLES
 
-// var name = "student"; // déclaration d'une variable
-// console.log(name);
-var name = 10; // réassignation avec `var`
-// console.log(name);
+// var → ancienne méthode (modifiable et redéclarable)
+// const → constante (non modifiable)
+// var nom = "Ali";
+// console.log(nom);
 
-// const name = "Foulen";
-// const age = 99;
-// console.log(age);
-// `var` peut être réassigné, mais `const` ne peut pas
-// var name;
-// name = "foulani";
-// console.log(name);
+// nom = 10; // réassignation
+// console.log("nom:", nom);
+
+// const prenom = "Foulen";
+// var age = 99;
+// // console.log(age);
+// // prenom = "Samiha";
+// // // var personne;
+// personne = "Foulani";
+// console.log(personne);
 // age = 100;
 // console.log(age);
 
-//*****************************************!Types de données
-
-// var name = "ahfgdhdh";
+//!TYPES DE DONNÉES
+// var name = "zez";
 // console.log(typeof name);
 // var age = 260;
 // console.log(typeof age);
 // var tallness = 1.75;
 // console.log(typeof tallness);
-// var isPerson = true;
+
+// var isPerson = false;
 // console.log(typeof isPerson);
 // var x = null;
 // console.log(typeof x);
 // var z;
 // console.log(typeof z);
 
-//**************************!Opérateurs arithmétiques
+// ! OPÉRATEURS ARITHMÉTIQUES
 // + , - , * , / , % , ++ , -- , **
-
 // console.log(13 - 2);
-// console.log("3" * "2");
-// console.log(typeof "3");
+// console.log("3" * 2);
 // console.log(3 * "TEXTE");
 // console.log("10" / "2");
-// console.log(10 % 2);
+// console.log(typeof "3");
 // console.log(10 % 3);
-// console.log("3" ** "2"); //la puissance
-// console.log(5 + 3);
-// console.log(5 + "a" + "agfertf"); //concaténation
+// console.log("3" ** "2");
+// console.log("5" + 2);
+// console.log(typeof (5 + "a"));
+// console.log(typeof ("" + 125));
 // var y = 5;
 // console.log(++y);
-// console.log(y);
 // console.log(y++);
 // console.log(y);
-// var z;
-// console.log(z);
-// z = ++y;
-// console.log(z);
-// console.log(y);
-// var t = y++;
-// console.log(t);
-// console.log(y);
 
-//******************!Opérateurs de comparaison
-// = affecte une valeur à une variable
-
-// == compare les valeurs des variables
-
-// === compare les valeurs ET les types des variables
-
-// /! le contraire (opposé)
-
-// /!= compare l'inégalité des valeurs
-
-// /!== compare l'inégalité des valeurs et des types
-
-// var a = 25; //affectation
+// ! OPÉRATEURS DE COMPARAISON
+// var a = 25;
 // var b = "25";
-// console.log(typeof a);
-// console.log(typeof b);
+// // console.log(typeof a, typeof b);
+
 // if (a === b) {
-//   console.log(a + " est égal en valeur et en type à " + b);
+//   //comparaison contenu et type ====
+//   console.log("Égal en valeur et type");
+// } else if (a == b) {
+//   //comparaison contenu
+//   console.log("Égal seulement en valeur");
 // } else {
-//   console.log("pas égal en type ou en valeur");
+//   console.log("Pas égal");
 // }
-// if (a == b) {
-//   console.log("égal ");
-// } else {
-//   console.log("pas égal");
-// }
+// console.log(a !== b);
+//=================================================
+// toDO:resume
+// = affectation d'une vvaleur à une variable
 
-//****************************!Truthy & Falsy
-// false, undefined, null, 0, "", NaN//falsy
+// == comparer les valeur des variables
 
-// var test = null;
-// var title = "";
-// var y; //undefined
-// console.log(typeof y);
-// var a = 0;
-// console.log(test);
-// console.log(Boolean(test));
-// console.log(Boolean(title));
-// console.log(Boolean(y));
-// console.log(Boolean(a));
-// if (test) {
-//   console.log('okay')
-// } else {
-//   console.log(' pas okay')
-// }
+// === comparer valeurs et types variables
 
-// ***********************************!Opérateurs logiques
-// ********** X && Y : ET logique
-// Renvoie la première valeur fausse
-//renvoie la deuxieme valeur si première vraie
-// console.log(null && 5);
-//  a && b  =>
-//  f    f  f
-//  v    f  f
-//  f    v  f
-//  v    v  v
+// / ! le contraire (opposite)
 
-// console.log(5 && 1);
+// /!= comparer l'inégalité des valeurs
 
-// ********** X || Y : OU logique
-// Renvoie la première valeur vraie
-// console.log("" || 0);
-//  a || b  =>
-//  f    f  f
-//  v    f  v
-//  f    v  v
-//  v    v  v
+// /!== compares the inequality based on value and type
 
-//***************!Concaténation de chaînes
-// var name = "hamza";
-// var age = 20;
+//!TRUTHY & FALSY
+// ======================================================
+// Falsy: false, undefined, null, 0, "", NaN
+// var t = 123;
+// console.log(Boolean(t));
+// console.log(Boolean(null));
+// console.log(Boolean(""));
+// console.log(Boolean(undefined));
+// console.log(Boolean(0));
+// console.log(Boolean("re" * 3));
+// console.log(Boolean(5 !== 5));
 
-// // Méthode archaïque traditionnelle
-// console.log("Je m'appelle " + name + "\n" + "et j'ai " + age + " ans");
+// if (null) console.log("Truthy");
 
-// // Littéraux de gabarit (utilisez ça!)
-// console.log(`Je m'appelle ${name}
-//     et
-//     j'ai
-//      ${2 + 3} ans`);
-// //``:backteks (altGr + 7)
-// //Caractères spéciaux (\, \', \n, \\)
+//!OPÉRATEURS LOGIQUES
 
-// console.log('je m"appelle sam');
+// // && → ET, || → OU, ! → NON
+// console.log(null && 5); //premier TRUE afficher le deuxieme
+// // a b     a && b
+// // 0 0       0
+// // 0 1       0
+// // 1 0       0
+// // 1 1       1
+// console.log(0 && 1);
+// // a b     a || b
+// // 0 0       0
+// // 0 1       1
+// // 1 0       1
+// // 1 1       1
+// console.log("" || 7); //premier faux afficher le premier
+// console.log(true || 4);
+// console.log(!false);
 
-//******!Méthodes et propriétés des chaînes de caractères **** */
+//!CONCATÉNATION DE CHAÎNES
+
+// var nom2 = "Hamza";
+// var age2 = 20;
+// console.log("je m'appelle " + nom2);
+// console.log('je mappelle "sam"');
+// // // es6
+// console.log(`Je m'appelle
+//     ${nom2}
+//     et j'ai ${age2}
+//     ans`);
+// ` ` Alt GR +7
+//!MÉTHODES DES CHAÎNES
+
 // var s = "Hello World of JS";
-// var val;
-// console.log(val);
-// console.log(typeof val);
-
-// // Obtenir la longueur
-// val = s.length;
 // console.log(s.length);
-// console.log(val);
-
-// // Index des chaînes
 // console.log(s[0]);
+
 // console.log(s[s.length - 1]);
+// console.log(s.toUpperCase());
+// console.log(s.toLowerCase());
+// console.log(s.substring(2, 10));
+// console.log(s.split(""));
+// console.log(["H", "i"].join(""));
 
-// Changer la casse
-// j === J? //différent
-// val = s.toUpperCase();
-// console.log(val);
-// val = s.toLowerCase();
-// console.log(val);
-// var s = "Hello World of JS";
-// // Extraire une sous-chaîne
-// val = s.substring(5, 10);
-// console.log(val);
-// var s = "Hello World";
-// // Diviser en tableau
-// val = s.split(""); // résultat tableau
-// console.log(val);
-// val = [1, 4, 5];
-// console.log(typeof val);
+//! CONDITIONS ET BOUCLES
 
-// val = val.join(""); // tableau ---> chaîne
-// console.log(val);
-// console.log(typeof val);
+//--------------If else /Switch------------
+// const x2 = 10;
+// if (x2 == 10) console.log("x est égal à 10");
+// else if (x2 > 10) console.log("x > 10");
+// else console.log("x < 10");
 
-// console.log(s);
-// console.log(val);
-
-//*****************!Switch (/!\break) */
-// var color = "noire";
+// var color = "jaune";
 // switch (color) {
 //   case "rouge":
 //     console.log("La couleur est rouge");
@@ -203,265 +165,117 @@ var name = 10; // réassignation avec `var`
 //     console.log("La couleur est jaune");
 //     break;
 //   default:
-//     console.log("La couleur n'est ni rouge ni bleue ni jaune");
-// }
-// // Reproduction avec if/else
-// if (couleur === "rouge") {
-//   console.log("La couleur est rouge");
-// } else if (couleur === "bleue") {
-//   console.log("La couleur est bleue");
-// } else if (couleur === "jaune") {
-//   console.log("La couleur est jaune");
-// } else {
-//   console.log("La couleur n'est ni rouge ni bleue ni jaune");
-// }
-// ************!FONCTIONS (en tant que type)
-// Déclaration de fonction
-// function dummyFunc() {
-//   return;
+//     console.log("Autre couleur");
 // }
 
-// // Vérification du type
-// console.log(typeof dummyFunc);
+// // traduction en if/else
+// if (color === "rouge") console.log("rouge");
+// else if (color === "bleue") console.log("bleue");
+// else if (color === "jaune") console.log("jaune");
+// else console.log("autre");
 
-//************      Fonctions       */
+// ---------------- For Loop ----------------
+// for (var i = 1; i <= 5; i++) console.log("For:", i);
+
+// var tab = [1, "p", "bonjour", true];
+
+// for (var i = 0; i < tab.length; i++) console.log(tab[i]);
+
+// for (var el of tab) console.log(el);
+// for (var i in tab) console.log(tab[i]);
+
+// ---------------- While & Do While ----------------
+// var l = 10;
+// while (l <= 12) {
+//   console.log("While:", l);
+//   l++;
+// }
+// var i = 0;
+// do {
+//   console.log(`Do While : ${i}`);
+//   i++;
+// } while (i <= 2);
+
+//!FONCTIONS
 // function salutation(n) {
-//   return "Bonjour, je m’appelle " + n;
+//   //   console.log("object");
+//   return `Bonjour, je m'appelle ${n}`;
 // }
-// function salutation(n) {
-//   return `Bonjour, je m'appelle ${n} `;
-// }
-
+// console.log(salutation("Sam"));
 // console.log(salutation("Nessrine"));
-// console.log(salutation("Wissem"));
-// console.log(salutation(5));
 
 // function soustraction(g, h) {
 //   return g - h;
 // }
-// var résultat = soustraction("5", "10");
-// console.log(résultat);
-// console.log(soustraction(4, 3));
-// console.log(soustraction("600a", 326587));
-//
-// var x = 10;
-// var y = 3;
-// console.log(soustraction(x, y)); //il faut déclarer ses var
+// console.log(soustraction(10, 3));
 
 // function addition(g, h) {
 //   return g + h;
 // }
 // console.log(addition("4", "3"));
 
-// Déclaration d’un tableau
-// var tableau = [];
-// console.log(tableau);
-// console.log(typeof tableau);
+//! TABLEAUX
 
-//************* */ Conditions
-// **** Instruction Simple If/Else
-// const x = "10";
-
-// if (x === 10) {
-//   console.log("x est égal à 10"); //❌
-// } else if (x > 10) {
-//   console.log("x est supérieur à 10"); //❌
-// } else {
-//   console.log("x est inférieur à 10");//sortie n'est valable
-// }
-
-//*********************!! Boucles
-// ********************** For
-// for (var i = 1; i <= 10; i++) {
-//   console.log("Numéro de la boucle For : " + i);
-// }
-
-// var tab = [1, "p", "bonjour", true];
-// // Boucle décroissante
-// for (var i = 0; i < tab.length; i++) {
-//   console.log(i);
-//   console.log(tab[i]);
-// }
-// for (var i = tab.length - 1; i >= 0; --i) {
-//   console.log(i);
-//   console.log(tab[i]);
-// }
-
-/*    for ... of   */
-// for (var element of tab) {
-//   /// valeur ou élément
-//   console.log(element);
-// }
-// /*       for ... in       */
-// for (var i in tab) {
-//   // index
-//   console.log(i);
-//   console.log(tab[i]);
-// }
-
-//*********************! While
-// var l = 10;
-// while (l <= 5) {
-//   console.log("hola");
-//   // Affiche "hola" tant que l < 5
-//   console.log(l);
-//   l++; //changement
-//   console.log(l);
-// }
-
-// var i = 0;
-// do {
-//   console.log(`Numéro de la boucle Do While : ${i}`);
-//   i++;
-// } while (i <= 5);
-
-// ************! TABLEAUX - Stocker plusieurs valeurs dans une variable
 // var numbers = [1, 2, 3, 4, 5];
 // var fruits = ["pommes", "oranges", "poires", "raisins"];
-// var hobbies = ["dormir", 33, true];
-
 // console.log(numbers);
-// console.log(fruits);
-// console.log(hobbies);
 // console.log(typeof numbers);
-
-// *** Modification des tableaux
-// const numbers = [1, 2, 3];
-// console.log(numbers);
-// // // Une nouvelle affectation
-// numbers[1] = "bonjour"; // Remplace l'élément à l'index 1
+// console.log(numbers[1]);
+// // // Modifier un élément
+// numbers[0] = "bonjour";
 // console.log(numbers);
 
-//********************! Méthodes et propriétés des tableaux
+// // Méthodes utiles
 // const colors = ["noir", "blanc", "rouge", "bleu"];
-// var x = "Bonjour";
-// console.log(colors);
-
-// // Obtenir la longueur
-// console.log(colors.length);
-
-// // // Obtenir une valeur (!: Les index commencent à 0)
-// console.log(colors[2]);
-
-// // // Ajouter une valeur avec push()
 // colors.push("jaune");
 // console.log(colors);
-
-// // // Ajouter une valeur au début
 // colors.unshift("violet");
 // console.log(colors);
-
-// // // Supprimer la dernière valeur
 // colors.pop();
 // console.log(colors);
-
-// // // Supprimer la première valeur
 // colors.shift();
 // console.log(colors);
-
-// // Vérifier si c’est un tableau
 // console.log(Array.isArray(colors));
-// console.log(Array.isArray(x));
-
-// // Obtenir un index
 // console.log(colors.indexOf("rouge"));
 
-// **********************! Parcourir des tableaux
-// // Tableau d'objets
-// const numbers = [22, 55, 66];
-
-// // // ********************** Boucle For
-// for (var i = 0; i < numbers.length; i++) {
-//   console.log(numbers[i]);
-// }
-
-// // // ********************** Boucle For...of
-// for (var x of numbers) {
-//   console.log(x);
-// }
-
-// // // ********************** Boucle For...in
-// for (var i in numbers) {
-//   console.log(numbers[i]);
-// }
-
-// ************! OBJETS
-// const person = {
-//   name: "Emna",
-//   age: 20,
-//   isPerson: true,
-//   hobbies: ["manger", "dormir"],
-// };
-
-// console.log(person);
-// console.log(typeof person);
-// console.log(person.name);
-// console.log(person.age);
-// console.log(person.hobbies[0]);
-
-//****************************!Objets
-// const person = {
-//   // un objet est une collection de paires clé-valeur
-//   firstName: "Adam",
-//   age: 30,
-//   hobbies: ["musique", "films", "sports"],
-//   address: {
-//     street: "66 rue Principale",
-//     city: "peu importe",
-//     state: "quelque part",
-//   },
-// };
-// var x = [1, [2, 5, 4], 3];
-// console.log(x[1][2]);
-// // // Obtenir une seule valeur (obj.clé)
-// console.log(person.firstName);
-// console.log(person.address.street);
-
-// // // Ajouter une propriété
-// person.email = "adam@gmail.com";
-// console.log(person);
-// person.hobbies.push("chanter");
-// console.log(person);
-
-// *** Mutabilité
 // *** Objets
-// const person = {
-//   name: "foulan",
-//   age: 27,
-//   adresse: "Tunis",
-// };
-// // console.log(person);
+const person = {
+  name: "foulan",
+  age: 27,
+  adresse: "Tunis",
+};
+console.log(person);
 
-// // Modifier une propriété
-// person.name = "mohamed";
-// console.log(person);
+// Modifier une propriété
+person.name = "mohamed";
+console.log(person);
 
-// // // // Supprimer une propriété
-// delete person.age;
-// console.log(person);
+// Supprimer une propriété
+delete person.age;
+console.log(person);
 
-// // // // Ajouter une propriété
-// person.email = "m@gmail.com";
-// console.log(person);
+// Ajouter une propriété
+person.email = "m@gmail.com";
+console.log(person);
 
 // ************ /!\ Tableau d'objets
-// const users = [
-//   {
-//     name: "personne",
-//     age: 23,
-//     email: "p1@gmail.com",
-//   },
-//   {
-//     name: "personne 2",
-//     age: 31,
-//     email: "p2@gmail.com",
-//   },
-//   {
-//     name: "personne 3",
-//     age: 155,
-//     email: "p3@gmail.com",
-//   },
-// ];
+const users = [
+  {
+    name: "personne",
+    age: 23,
+    email: "p1@gmail.com",
+  },
+  {
+    name: "personne 2",
+    age: 31,
+    email: "p2@gmail.com",
+  },
+  {
+    name: "personne 3",
+    age: 155,
+    email: "p3@gmail.com",
+  },
+];
 
-// console.log(users);
-// console.table(users);
+console.log(users);
+console.table(users);
